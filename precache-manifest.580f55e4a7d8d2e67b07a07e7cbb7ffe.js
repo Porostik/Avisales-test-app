9af6a55e38d38b4346ab015e6f6ded0019a0791b
Porostik <porostik2@yandex.ru>
2020-10-17 18:57:41 +0300

    Updates

---
+++ precache-manifest.580f55e4a7d8d2e67b07a07e7cbb7ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50d5fce9be37c493c118b1f1eee9919",
+    "url": "/Avisales-test-app/index.html"
+  },
+  {
+    "revision": "9be2f7288e92e39ff6bb",
+    "url": "/Avisales-test-app/static/css/main.e527cbe5.chunk.css"
+  },
+  {
+    "revision": "91133ceda80b7991132e",
+    "url": "/Avisales-test-app/static/js/2.6d7c8fc9.chunk.js"
+  },
+  {
+    "revision": "3043e3e727d80f353557f855ab1ce92b",
+    "url": "/Avisales-test-app/static/js/2.6d7c8fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be2f7288e92e39ff6bb",
+    "url": "/Avisales-test-app/static/js/main.25c847ab.chunk.js"
+  },
+  {
+    "revision": "f7f49f2c50eaa673a5ae",
+    "url": "/Avisales-test-app/static/js/runtime-main.bf9daeca.js"
+  },
+  {
+    "revision": "5d1dfa0eb3e988824ef1802f99f0974e",
+    "url": "/Avisales-test-app/static/media/logo.5d1dfa0e.svg"
+  }
+]);
